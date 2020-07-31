8ba0b6c0a481f95e8048681dfb1ee9cc52d11710
Wasurat Thampanya <wasurat.th@baac.or.th>
2020-07-31 10:20:11 +0700

    add knowledge api

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -9,6 +9,23 @@ module.exports.getMyProfile = async (req, res , next ) => {
     res.status(200).json({ data: user , success: true , msg : '' } );
 }
 
+module.exports.changeMyPassword = async (req, res , next ) => {
+    const { _id : user_id } =  req.user ; 
+    const { oldpassword ,  newpassword  } =  req.body ; 
+    
+    const user = await User.findById(user_id) ; 
+    
 
+    try {
+        if(user.password !== await user.encryptPassword(oldpassword)){
+            res.status(404).json({ success: false , data: {}  , msg : "รหัสผ่านเดิมไม่ถูกต้อง" } );
+        }
+        
+        user.password = user.encryptPassword(oldpassword) ; 
+        user.save();
+        res.status(200).json({ success: true , data: {}  , msg : "" } );
+    }catch (err) {
+        next(err);
+    }
 
- 
+}
